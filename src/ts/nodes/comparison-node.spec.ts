7609ba27d898b41176105f33161b4d900dd645e7
Jérémie Piellard <jeremie.piellard@gmail.com>
2019-11-18 22:00:44 +0100

    Added description to comparison node unit tests

--- src/ts/nodes/comparison-node.spec.ts
+++ src/ts/nodes/comparison-node.spec.ts
@@ -5,29 +5,31 @@ function testConversion(expected: EComparison, righthand: string): void {
     expect(node.comparison).toBe(expected);
 }
 
-test("Equals conversion to EQUALS", function () {
-    testConversion(EComparison.EQUALS, "");
-    testConversion(EComparison.EQUALS, "haha");
-});
+describe("Comparison node: conversion of EQUALS operator", function () {
+    test("Equals conversion to EQUALS", function () {
+        testConversion(EComparison.EQUALS, "");
+        testConversion(EComparison.EQUALS, "haha");
+    });
 
-test("Equals conversion to STARTS_WITH", function () {
-    testConversion(EComparison.STARTS_WITH, "*haha");
-    testConversion(EComparison.STARTS_WITH, "****haha");
-});
+    test("Equals conversion to STARTS_WITH", function () {
+        testConversion(EComparison.STARTS_WITH, "*haha");
+        testConversion(EComparison.STARTS_WITH, "****haha");
+    });
 
-test("Equals conversion to ENDS_WITH", function () {
-    testConversion(EComparison.ENDS_WITH, "haha*");
-    testConversion(EComparison.ENDS_WITH, "haha*****");
-});
+    test("Equals conversion to ENDS_WITH", function () {
+        testConversion(EComparison.ENDS_WITH, "haha*");
+        testConversion(EComparison.ENDS_WITH, "haha*****");
+    });
 
-test("Equals conversion to CONTAINS", function () {
-    testConversion(EComparison.CONTAINS, "*haha*");
-    testConversion(EComparison.CONTAINS, "*haha*****");
-    testConversion(EComparison.CONTAINS, "******haha*");
-    testConversion(EComparison.CONTAINS, "******haha*****");
-});
+    test("Equals conversion to CONTAINS", function () {
+        testConversion(EComparison.CONTAINS, "*haha*");
+        testConversion(EComparison.CONTAINS, "*haha*****");
+        testConversion(EComparison.CONTAINS, "******haha*");
+        testConversion(EComparison.CONTAINS, "******haha*****");
+    });
 
-test("Equals conversion to EXISTS", function () {
-    testConversion(EComparison.EXISTS, "*");
-    testConversion(EComparison.EXISTS, "******");
+    test("Equals conversion to EXISTS", function () {
+        testConversion(EComparison.EXISTS, "*");
+        testConversion(EComparison.EXISTS, "******");
+    });
 });
